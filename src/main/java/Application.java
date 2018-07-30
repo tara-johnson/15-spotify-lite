57bbfb6c457ca76607541faa98a8f3cda38ed706
Tara Johnson <tara@madface.com>
2018-07-30 12:51:25 -0700

    Project setup. DB setup in progress.

---
+++ src/main/java/Application.java
@@ -0,0 +1,23 @@
+package server;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import server.storage.FileSystemStorageService;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("http://localhost:8080");
+    }
+
+    @Bean
+    CommandLineRunner init(FileSystemStorageService storageService) {
+        return (args) -> {
+            storageService.init();
+        };
+    }
+}
