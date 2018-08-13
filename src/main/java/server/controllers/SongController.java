378974f7bb6b0288f6d0ec51c3d0ab8e3f5f66f2
Tara Johnson <tara@madface.com>
2018-08-13 13:17:00 -0700

    Styling index and upload pages.

--- src/main/java/server/controllers/SongController.java
+++ src/main/java/server/controllers/SongController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import server.db.MockDB;
 import server.db.SongDB;
 import server.models.Song;
 
